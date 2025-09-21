d063497815850d1d1d11620d57f5d0cd54b4c44d
Alexey Savchenko <alexey.repository@gmail.com>
2025-09-21 13:18:44 +0300

    fix tests

--- internal/domain/product/product_test.go
+++ internal/domain/product/product_test.go
@@ -1,33 +1,50 @@
 package product
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/alexey-savchenko-am/shop-ddd/internal/domain/common"
+)
 
 func TestNewProduct(t *testing.T) {
-	_, err := New("p1", "SKU-1", "Product 1", Price(1000))
+
+	price, err := common.NewUsd(1000)
+
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	_, err = New("SKU-1", "Product 1", price)
+
 	if err != nil {
 		t.Fatalf("expected no error, got %v", err)
 	}
 }
 
 func TestInvalidProduct(t *testing.T) {
-	_, err := New("", "", "", 0)
+
+	price, _ := common.NewUsd(0)
+
+	_, err := New("", "", price)
+
 	if err == nil {
 		t.Fatal("expected error, got nil")
 	}
 }
 
 func TestChangePrice(t *testing.T) {
-	p, _ := New("p1", "SKU-1", "Product 1", Price(1000))
 
-	if err := p.ChangePrice(2000); err != nil {
+	price, _ := common.NewUsd(1000)
+
+	p, _ := New("SKU-1", "Product 1", price)
+
+	newPrice, _ := common.NewUsd(2000)
+
+	if err := p.ChangePrice(newPrice); err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	if p.Price() != 2000 {
-		t.Fatalf("expected price 2000, got %d", p.Price())
-	}
 
-	if err := p.ChangePrice(0); err == nil {
-		t.Fatalf("expected error, got nil")
+	if p.Price().Amount != newPrice.Amount {
+		t.Fatalf("expected price 2000, got %d", p.Price().Amount)
 	}
 }
-
